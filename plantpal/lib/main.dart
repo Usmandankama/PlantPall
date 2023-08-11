98740f627497b28117cc8c0c2e31ab9b1606fa74
Usman <104586608+Usmandankama@users.noreply.github.com>
2023-08-11 19:33:58 +0100

    Added some resources

--- plantpal/lib/main.dart
+++ plantpal/lib/main.dart
@@ -13,120 +13,100 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Plant Shop',
       theme: ThemeData(
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      // Set the initial route to the HomeScreen
-      initialRoute: '/',
-      // Define routes to navigate between pages
-      routes: {
-        '/': (context) => const HomeScreen(),
-        // '/login': (context) => LoginScreen(),
-        '/signup': (context) => SignupScreen(),
-      },
+      home:const StartUpScreen()
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class StartUpScreen extends StatefulWidget {
+  const StartUpScreen({super.key});
 
+  @override
+  State<StartUpScreen> createState() => _StartUpScreenState();
+}
+
+class _StartUpScreenState extends State<StartUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Plant Shop'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.account_circle),
-            onPressed: () {
-              // Navigate to the login page when the account icon is tapped
-              Navigator.pushNamed(context, '/login');
-            },
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            // Featured products section
-            _buildSectionTitle('Featured Products'),
-            _buildProductList(), // Replace with a product list widget
-
-            // Categories section
-            _buildSectionTitle('Categories'),
-            _buildCategoryList(), // Replace with a category list widget
-          ],
-        ),
-      ),
-    );
-  }
-
-  // Helper method to build section titles
-  Widget _buildSectionTitle(String title) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    );
-  }
-
-  // Helper method to build a mock product list
-  Widget _buildProductList() {
-    // In a real app, you'd fetch product data and create product cards dynamically
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: 5, // Replace with the actual product count
-      itemBuilder: (context, index) {
-        // Mock product card
-        return ListTile(
-          leading: const Icon(Icons.shopping_cart),
-          title: Text('Product ${index + 1}'),
-          subtitle: Text('Description of Product ${index + 1}'),
-          trailing: Text('\$${(index + 1) * 10}'),
-        );
-      },
-    );
-  }
-
-  // Helper method to build a mock category list
-  Widget _buildCategoryList() {
-    // In a real app, you'd fetch category data and create category cards dynamically
-    return Wrap(
-      spacing: 16,
-      runSpacing: 16,
-      children: List.generate(
-        8, // Replace with the actual category count
-        (index) {
-          // Mock category card
-          return InkWell(
-            onTap: () {
-              // Handle category selection
-              print('Category $index tapped');
-            },
-            child: Container(
-              width: 100,
-              height: 100,
-              color: Colors.blue,
-              alignment: Alignment.center,
-              child: Text(
-                'Category $index',
-                style: const TextStyle(color: Colors.white),
-              ),
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("./assets/images/Vayu.jpg"),
+              fit: BoxFit.cover,
             ),
-          );
-        },
-      ),
-    );
+          ),
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 160, bottom: 10),
+                child: Center(
+                  child: Text(
+                    "PlantPal",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 80,
+                        fontFamily: "Cursive",
+                        fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ),
+              const Text(
+                "Your personal",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700),
+              ),
+              const Text(
+                "Plant care helper",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 80),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()));
+                    },
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                      child: Text("Login"),
+                    )),
+              ),
+              ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll(Colors.grey.withOpacity(.6)),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SignupScreen()));
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 26, vertical: 12),
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+      );
   }
 }
-
-// ... LoginScreen and SignupScreen remain the same as in the previous example ...
--- plantpal/lib/screens/signupScreen.dart
+++ plantpal/lib/screens/signupScreen.dart
@@ -1,70 +1,75 @@
-// ignore_for_file: file_names
-
 import 'package:flutter/material.dart';
+import 'package:plantpal/screens/login_page.dart';
 
-import 'login_page.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Plant Login',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginScreen(),
-        '/signup': (context) => SignupScreen(),
-      },
-    );
-  }
-}
-
-class LoginScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Plant Login'),
-      ),
-      body: Center(
-        child: LoginForm(),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.login),
-            label: 'Login',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.person_add),
-            label: 'Signup',
-          ),
-        ],
-        onTap: (index) {
-          if (index == 1) {
-            Navigator.pushNamed(context, '/signup');
-          }
-        },
-      ),
-    );
-  }
-}
+import '../main.dart';
 
 class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
   @override
   _SignupScreenState createState() => _SignupScreenState();
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("./assets/images/catamaran.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const StartUpScreen()));
+                  },
+                  child: const Text(
+                    "PlantPal",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 80,
+                        fontFamily: "Cursive",
+                        fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(1),
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(50),
+                  topLeft: Radius.circular(50),
+                ),
+              ),
+              child: const Center(
+                child: SignupForm(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignupForm extends StatefulWidget {
+  const SignupForm({super.key});
+
+  @override
+  _SignupFormState createState() => _SignupFormState();
+}
+
+class _SignupFormState extends State<SignupForm> {
   final _formKey = GlobalKey<FormState>();
 
   String _email = '';
@@ -82,67 +87,80 @@ class _SignupScreenState extends State<SignupScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Plant Signup'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    _email = value;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    _password = value;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Confirm Password'),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please confirm your password';
-                    }
-                    if (value != _password) {
-                      return 'Passwords do not match';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    _confirmPassword = value;
-                  },
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: const Text('Signup'),
-                ),
-              ],
+    return Container(
+      height: 370,
+      margin: const EdgeInsets.symmetric(horizontal: 30),
+      padding: const EdgeInsets.all(20),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Center(
+              child: Text(
+                "Sign Up",
+                style: TextStyle(fontSize: 30, fontFamily: "Monospace"),
+              ),
             ),
-          ),
+            const SizedBox(height: 5),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Email'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your email';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                _email = value;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                _password = value;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Confirm Password'),
+              obscureText: true,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please confirm your password';
+                }
+                if (value != _password) {
+                  return 'Passwords do not match';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                _confirmPassword = value;
+              },
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _submitForm,
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                child: Text('Signup'),
+              ),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginScreen()));
+                },
+                child: const Text("Already have an account? Login"))
+          ],
         ),
       ),
     );
